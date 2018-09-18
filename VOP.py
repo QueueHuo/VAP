7342e5aa827f9f127d5e4943cd7178c7e76d1355
QueueHuo <465879542@qq.com>
2018-09-18 08:41:03 +0800

    Add files via upload
    improved

--- VOP.py
+++ VOP.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import TimeoutException
 #this can hopefully help gather the info from merriam-webster website
 #made by Queue Huo Sep.17 2018
 #CODE STYLE EVEN MORE TERRIBLE( See "code style really bad' from Alan?Liang)
@@ -9,8 +10,7 @@ fileWrite = open("Wordlist-meaningGathered.txt","w")
 chrome_opt = webdriver.ChromeOptions()
 prefs = {"profile.managed_default_content_settings.images": 2}
 chrome_opt.add_experimental_option("prefs", prefs)
-browser = webdriver.Chrome(executable_path="chromedriver.exe", chrome_options=chrome_opt)
-mainDriver = webdriver.Chrome()
+mainDriver = webdriver.Chrome(executable_path="chromedriver.exe", chrome_options=chrome_opt)
 
 currentWordCounter = 0
 keywords = fileRead.readlines()
@@ -19,19 +19,32 @@ fileWrite.flush()
 for keyword in keywords:
     print("#DEBUG:" + keyword + "\n")
     #fileWrite.write("\n")
-    mainDriver.get("https://www.merriam-webster.com/words-at-play/new-words-in-the-dictionary-september-2018")
-    inputbar = mainDriver.find_element_by_id("s-term")
-    inputbar.send_keys(keyword,Keys.RETURN)
-    results = mainDriver.find_elements_by_class_name("sense")
-    currentWordCounter += 1
-    fileWrite.write(str(currentWordCounter))
-    fileWrite.write(" " + keyword)
-    fileWrite.write(" : ")
-    for result in results:
-            fileWrite.write(result.text)
-            print(result.text + '\n')
-            fileWrite.flush()
+    try:
+        try:
+            mainDriver.get("https://www.merriam-webster.com/words-at-play/new-words-in-the-dictionary-september-2018")
+        except TimeoutException as e:
+            print("#DEBUG: Time out on website: ")
+            print(type(e))
+        try:
+            inputbar = mainDriver.find_element_by_id("s-term")
+            inputbar.send_keys(keyword,Keys.RETURN)
+            results = mainDriver.find_elements_by_class_name("sense")
+        
+            currentWordCounter += 1
+            fileWrite.write(str(currentWordCounter))
+            fileWrite.write(" " + keyword)
+            for result in results:
+                fileWrite.write("\t" + result.text + '\n')
+                print(result.text + '\n')
+                fileWrite.flush()
+        except TimeoutException as e:
+            print("#DEBUG: Time out on crawl page: ")
+            print(type(e))
+    except Exception as e:
+        print("#DEBUG: A serious accident has occured, making Chromedriver forced to close")
+        mainDriver.close()
     #print(keyword)
 fileRead.close()
 fileWrite.close()
+mainDriver.close()
 
